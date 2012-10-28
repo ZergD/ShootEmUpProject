f1848c23cde3bf10e9e65d334e4a1027c5a83a4b
Zerg <marcmozgawa@gmail.com>
2012-10-28 22:00:34 +0100

    We have a simple object that we can move with arrows

--- test.cpp
+++ test.cpp
@@ -8,40 +8,29 @@
  */
 void affiche()
 {
-	/* pour indiquer la fin de l'affichage */
-	int display;
-
-	/* pour stocker les evenements */
-	SDL_Event event;
+	int display; /* pour indiquer la fin de l'affichage */
+	int dx = 0;
+	int dy = 0;
+	SDL_Event event; /* pour stocker les evenements */
 
 	/* couleur noire pour le fond de la fenetre */
 	Uint8 fond = build_color(0x00, 0x00, 0x00);
-
+	Uint8 blanc = build_color(250, 250, 250);
 	/* pour indiquer si on doit quitter */
 	display = 1;
 	while(display)
 	{
-		/*
-		 * s'il y a des evenements en attente, on les gere
-		 */
+		/* s'il y a des evenements en attente, on les gere*/
 		while (SDL_PollEvent(&event))
-		{
-			/*
-			 * si l'utilisateur a demande a fermer la fenetre, on
-			 * quitte le programme
-			 */ 
+		{			
+			/* si l'utilisateur a demande a fermer la fenetre, on quitte le programme*/
 			if (event.type == SDL_QUIT)
 				display=0;
 			
-			/*
-			 * si l'utilisateur a appuye sur une touche...
-			 */
+			/* si l'utilisateur a appuye sur une touche...*/
 			if (event.type == SDL_KEYDOWN)
 			{
-				/**
-				 * on quitte si l'utilisateur a appuye sur la
-				 * touche q (pour "quitter")
-				 */
+				/* on quitte si l'utilisateur a appuye sur la touche q (pour "quitter")*/
 				switch (event.key.keysym.sym)
 				{
 					case SDLK_q :
@@ -50,50 +39,43 @@ void affiche()
 
 					case SDLK_UP :
 						printf("appui sur la fleche du haut\n");
+						dy += -10;
+						break;
+					case SDLK_DOWN :
+						printf("appui sur la fleche du bas\n");
+						dy += 10;
+						break;
+					case SDLK_LEFT :
+						printf("appui sur la fleche de gauche\n");
+						dx += -10;
+						break;
+					case SDLK_RIGHT :
+						printf("appui sur la fleche de droite\n");
+						dx += 10;
 						break;
 
 					default :
 						printf("appui sur une autre touche\n");
-				
-					/**
-					 * voir les autres codes de caracteres sur la page :
-					 * http://sdldoc.csn.ul.ie/sdlkey.php
-					 */
 				}
 			}
-			/**
-			 * voir les autres types d'evenements
-			 */
-		}
-		
+		}		
 		/* on verrouille l'ecran */
 		lock();
 
-
-		/*
-		 * on remplit la fenetre avec la couleur cree au debut
-		 * de la fonction
-		 */
+		/* on remplit la fenetre avec la couleur cree au debut de la fonction */
 		fill_screen(fond);
 
-		/*
-		 * on dessine un triangle de couleur aleatoire
-		 */
-		fill_triangle(257,177,
-		              384,172,
-		              278,198,
-		              build_color(255,0,0));
-		
+		/* on dessine un triangle*/
+		fill_triangle(WIDTH/2 + dx     ,HEIGHT/2 + dy,
+				      WIDTH/2 - 25 + dx,HEIGHT - 100 + dy,
+					  WIDTH/2 + 25 + dx,HEIGHT - 100 + dy,
+					  blanc);
 		//fill_circle(10, 20, 50, random());
-
 
 		/* on deverouille et on rafraichit l'ecran */
         	unlock();
 
-
-		/* on attend le temps necessaire pour atteindre le taux de
-		 * rafraichissement souhaite
-		 */
+		/* on attend le temps necessaire pour atteindre le taux de rafraichissement souhaite */
 		 sync();
 	}
 	
@@ -111,3 +93,36 @@ int main(int argc, char * argv[])
 	printf("on quitte\n");
 	exit(0);
 }
+
+/*
+int main(int argc, char *argv[]) {
+    int continuer = 1;
+	SDL_Surface *ecran = NULL;
+    SDL_Event event; // Cette variable sert à gerer les evenements
+
+    SDL_Init(SDL_INIT_VIDEO);
+	
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Gestion des evenements en SDL", NULL);
+
+
+	while(continuer) {
+		SDL_WaitEvent(&event);
+		switch (event.type) {
+			case SDL_QUIT : 
+				continuer = 0;
+				break;
+			case SDL_KEYDOWN :
+				switch(event.key.keysym.sym) {
+					case SDLK_ESCAPE : continuer = 0;  // SDLK_UP == up arrow
+						break;
+				}
+				break;
+		}	
+	}
+
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+*/
