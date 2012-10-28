6d987dd366196e6fecac7ea4280b70fd2778c848
Zerg <marcmozgawa@gmail.com>
2012-10-28 23:57:22 +0100

    Fixed the movement!
    tweak: Now you just have to hold down the key to move da spaceship !

--- test.cpp
+++ test.cpp
@@ -8,16 +8,23 @@
  */
 void affiche()
 {
-	int display; /* pour indiquer la fin de l'affichage */
-	int dx = 0;
-	int dy = 0;
+	int display = 0; /* pour indiquer la fin de l'affichage */
 	SDL_Event event; /* pour stocker les evenements */
+	bool toucheAppuyeeUp = false;
+	bool toucheAppuyeeDown = false;
+	bool toucheAppuyeeLeft = false;
+	bool toucheAppuyeeRight = false;
+
+	int positionX = WIDTH / 2;
+	int positionY = HEIGHT / 2;
 
 	/* couleur noire pour le fond de la fenetre */
 	Uint8 fond = build_color(0x00, 0x00, 0x00);
 	Uint8 blanc = build_color(250, 250, 250);
+	Uint8 rouge = build_color(250, 0, 0);
 	/* pour indiquer si on doit quitter */
 	display = 1;
+
 	while(display)
 	{
 		/* s'il y a des evenements en attente, on les gere*/
@@ -28,7 +35,7 @@ void affiche()
 				display=0;
 			
 			/* si l'utilisateur a appuye sur une touche...*/
-			if (event.type == SDL_KEYDOWN)
+			if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
 			{
 				/* on quitte si l'utilisateur a appuye sur la touche q (pour "quitter")*/
 				switch (event.key.keysym.sym)
@@ -39,19 +46,19 @@ void affiche()
 
 					case SDLK_UP :
 						printf("appui sur la fleche du haut\n");
-						dy += -10;
+						toucheAppuyeeUp = event.type == SDL_KEYDOWN;
 						break;
 					case SDLK_DOWN :
 						printf("appui sur la fleche du bas\n");
-						dy += 10;
+						toucheAppuyeeDown = event.type == SDL_KEYDOWN;
 						break;
 					case SDLK_LEFT :
 						printf("appui sur la fleche de gauche\n");
-						dx += -10;
+						toucheAppuyeeLeft = event.type == SDL_KEYDOWN;
 						break;
 					case SDLK_RIGHT :
 						printf("appui sur la fleche de droite\n");
-						dx += 10;
+						toucheAppuyeeRight = event.type == SDL_KEYDOWN;
 						break;
 
 					default :
@@ -59,6 +66,20 @@ void affiche()
 				}
 			}
 		}		
+		
+		if(toucheAppuyeeUp) {
+			positionY += -10;
+		}
+		if(toucheAppuyeeDown) {
+			positionY += +10;
+		}
+		if(toucheAppuyeeLeft) {
+			positionX += -10;
+		}
+		if(toucheAppuyeeRight) {
+			positionX += 10;
+		}
+
 		/* on verrouille l'ecran */
 		lock();
 
@@ -66,10 +87,11 @@ void affiche()
 		fill_screen(fond);
 
 		/* on dessine un triangle*/
-		fill_triangle(WIDTH/2 + dx     ,HEIGHT/2 + dy,
-				      WIDTH/2 - 25 + dx,HEIGHT - 100 + dy,
-					  WIDTH/2 + 25 + dx,HEIGHT - 100 + dy,
-					  blanc);
+		fill_triangle(positionX + 25 , positionY ,
+					  positionX - 25 , positionY ,
+					  positionX      , positionY - 100,
+						  blanc);	
+
 		//fill_circle(10, 20, 50, random());
 
 		/* on deverouille et on rafraichit l'ecran */
@@ -93,36 +115,3 @@ int main(int argc, char * argv[])
 	printf("on quitte\n");
 	exit(0);
 }
-
-/*
-int main(int argc, char *argv[]) {
-    int continuer = 1;
-	SDL_Surface *ecran = NULL;
-    SDL_Event event; // Cette variable sert à gerer les evenements
-
-    SDL_Init(SDL_INIT_VIDEO);
-	
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
-    SDL_WM_SetCaption("Gestion des evenements en SDL", NULL);
-
-
-	while(continuer) {
-		SDL_WaitEvent(&event);
-		switch (event.type) {
-			case SDL_QUIT : 
-				continuer = 0;
-				break;
-			case SDL_KEYDOWN :
-				switch(event.key.keysym.sym) {
-					case SDLK_ESCAPE : continuer = 0;  // SDLK_UP == up arrow
-						break;
-				}
-				break;
-		}	
-	}
-
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
-}
-*/
